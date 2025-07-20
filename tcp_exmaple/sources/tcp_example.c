8e55ce73dd576df950ecbf5a337a1fb7010a7e4e
alic3 <alic3dev@gmail.com>
2025-07-20 03:25:17 -0700

    `argument`->`parameter`;

--- tcp_exmaple/sources/tcp_example.c
+++ tcp_exmaple/sources/tcp_example.c
@@ -9,22 +9,22 @@
 #include <stdio.h>
 
 int main(
-  int arguments_length, 
-  char** arguments
+  int length_parameters, 
+  char** parameters
 ) {
   struct tcp_example_parameters tcp_example_parameters;
   tcp_example_parameters.mode = server;
 
   for (
-    int arguments_index = 1;
-    arguments_index < arguments_length;
-    ++arguments_index
+    int parameters_index = 1;
+    parameters_index < length_parameters;
+    ++parameters_index
   ) {
-    switch(arguments_index) {
+    switch(parameters_index) {
       case 1:
         int index_client_server = clic3_char_arrays_within(
-          arguments[
-            arguments_index
+          parameters[
+            parameters_index
           ],
           2,
           "client",
@@ -41,9 +41,9 @@ int main(
       default:
         fprintf(
           stderr,
-          "unknown_argument: %s\n", 
-          arguments[
-            arguments_index
+          "unknown_parameter: %s\n", 
+          parameters[
+            parameters_index
           ]
         );
 
